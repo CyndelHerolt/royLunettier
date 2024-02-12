852109a7138d4de402b26e2c728a2234aca19a6d
CyndelHerolt <herolt.cyndel@gmail.com>
2024-02-12 16:22:58 +0100

    feat: first pages

--- src/views/DemarcheView.vue
+++ src/views/DemarcheView.vue
@@ -3,12 +3,21 @@
 
 <template>
   <main>
-    <div class="home_head">
-      <h1>Démarche</h1>
-    </div>
+    <h2>Démarche</h2>
+    <p>
+      <em>
+        Nous allons proposer à l'entreprise Roy Lunetier un outil qui permettra de mettre en exergue leurs valeurs
+        fortes.
+      </em>
+    </p>
+
+    <p>
+      En prenant en compte le fait que l'entreprise ne propose pas de création sur-mesure mais se limite plutôt à la
+      vente en BtoB auprès de revendeurs (majoritairement des opticiens indépendants), nous avons décidé de nous
+      orienter vers la création d'une interface qui sublimera l'utilisation des "produits oubliés".
+    </p>
   </main>
 </template>
 
 <style scoped>
-
 </style>
