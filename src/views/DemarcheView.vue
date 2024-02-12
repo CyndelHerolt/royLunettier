30420f6dc0702a5d25bd4edb222b39a99ee65710
CyndelHerolt <herolt.cyndel@gmail.com>
2024-02-12 18:47:42 +0100

    feat: first pages

--- src/views/DemarcheView.vue
+++ src/views/DemarcheView.vue
@@ -3,21 +3,50 @@
 
 <template>
   <main>
-    <h2>Démarche</h2>
-    <p>
-      <em>
-        Nous allons proposer à l'entreprise Roy Lunetier un outil qui permettra de mettre en exergue leurs valeurs
-        fortes.
-      </em>
-    </p>
+    <section>
+      <article>
+        <h2>Démarche</h2>
+        <p>
+          <em>
+            Nous allons proposer à l'entreprise Roy Lunetier un outil qui permettra de mettre en exergue leurs valeurs
+            fortes.
+          </em>
+        </p>
+        <p>
+          En prenant en compte le fait que l'entreprise ne propose pas de création sur-mesure mais se limite plutôt à la
+          vente en BtoB majoritairement auprès d'opticiens indépendants, nous avons décidé de nous
+          orienter vers la création d'une interface qui sublimera l'utilisation des "produits oubliés", un concept qui
+          fait la fierté de Roy Lunetier. L'idée ici est de proposer une découverte des caractéristiques et des points
+          forts des matériaux utilisés afin de valoriser les valeurs de la fabrication artisanale, durable et locale
+          défendues par l'entreprise.
+        </p>
+      </article>
+      <figure>
+        <img src="/logo_roy_lunetier_black.svg" alt="logo de roy lunetier">
+        <figcaption>
 
-    <p>
-      En prenant en compte le fait que l'entreprise ne propose pas de création sur-mesure mais se limite plutôt à la
-      vente en BtoB auprès de revendeurs (majoritairement des opticiens indépendants), nous avons décidé de nous
-      orienter vers la création d'une interface qui sublimera l'utilisation des "produits oubliés".
-    </p>
+        </figcaption>
+      </figure>
+    </section>
   </main>
 </template>
 
 <style scoped>
+section {
+  display: flex;
+}
+
+article {
+  width: 50%;
+}
+
+figure {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 40%;
+  }
+}
 </style>
