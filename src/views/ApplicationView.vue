bfc29090621c8aac366ae7e665718fa3ffae39ba
CyndelHerolt <herolt.cyndel@gmail.com>
2024-04-10 08:48:20 +0200

    coucou

--- src/views/ApplicationView.vue
+++ src/views/ApplicationView.vue
@@ -11,6 +11,12 @@
             Proof Of Concept
           </em>
         </p>
+        <a href="https://roy-lunetier-3d.vercel.app">
+          Lien vers l'outil
+        </a>
+        <figure style="width: 70vw; margin: 50px">
+          <iframe src="/public/demo.mp4" width="100%" height="550px" frameborder="0"></iframe>
+        </figure>
       </article>
     </section>
   </main>
